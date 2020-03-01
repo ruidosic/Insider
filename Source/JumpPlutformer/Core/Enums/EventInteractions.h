10e98f0351750a2b17139aef729d9ce40cea4428
Богдан Левченко <bodya.levchenko20@gmail.com>
2020-03-01 19:13:19 +0200

    no message

--- Source/JumpPlutformer/Core/Enums/EventInteractions.h
+++ Source/JumpPlutformer/Core/Enums/EventInteractions.h
@@ -13,7 +13,7 @@ enum EEventInteractions
 	EI_None				UMETA(DisplayName = "Nothing"),
 	EI_Use				UMETA(DisplayName = "Use"),
 	EI_AlternativeUse	UMETA(DisplayName = "AlternativeUse"),
-	EI_OpenON			UMETA(DisplayName = "Open / On"),
+	EI_OpenOn			UMETA(DisplayName = "Open / On"),
 	EI_CloseOff			UMETA(DisplayName = "Close / Off"),
 	EI_Lock				UMETA(DisplayName = "Lock"),
 	EI_Unlock			UMETA(DisplayName = "Unlock")
